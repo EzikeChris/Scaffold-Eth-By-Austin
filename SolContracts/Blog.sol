c72029bbc28b456419b34d05a33a735c8d10cfcc
ezikechris <christophermbe1009@gmail.com>
2021-04-22 15:50:00 +0100

    created solc folder

---
+++ SolContracts/Blog.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.4.26;
+contract YourContract{
+// Blog Post 
+// Blog Tittle author date Id etc
+
+
+struct Article {
+  string tittle;
+  string content;
+  address author;
+  uint256 timestamp;
+  uint256 id;
+}
+// create an array of article which would hold user data with there respective id
+Article[] public articles;
+// Create your contract body with publish article
+function publishArticle(string memory _tittle, string memory _content) public returns(uint256){
+/// user created article with user address content and timestamp with user id
+  uint256 myId = articles.length;
+  Article memory myArticle = Article(_tittle, _content, msg.sender, now, myId);
+  articles.push(myArticle);
+/// Return the id to the users
+  return myId;
+  
+}
+
+}
