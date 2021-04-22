bca157a03ac53ca4478a7c4f79c0381659ef94f0
ezikechris <christophermbe1009@gmail.com>
2021-04-22 14:27:34 +0100

    added blog contract

---
+++ Use.md
@@ -0,0 +1,33 @@
+``` HOW TO USE / RUN SCAFFOLD ETH FOR YOUR DEVELOPMENT ````
+
+# cd scaffold-eth
+
+````
+
+```bash
+
+yarn install
+
+````
+
+```bash
+
+>  Lunch Your front- end applications on localhost:3000:
+yarn start
+
+```
+
+> in a second terminal window lunch your local node blockchain on hardhat:
+
+```bash
+cd scaffold-eth
+yarn chain
+
+```
+
+> in a third terminal window deploy your smartContract to the front-end using the command:
+
+```bash
+cd scaffold-eth
+yarn deploy
+```
--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,22 +2,33 @@ pragma solidity >=0.6.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
-contract YourContract {
 
-  event SetPurpose(address sender, string purpose);
 
-  string public purpose = "Building Unstoppable Apps";
+contract YourContract{
+  // Blog Post 
+  // Blog Tittle author date Id etc
 
-  constructor() {
-    // what should we do on deploy?
-  }
 
-  function setPurpose(string memory newPurpose) public {
-    purpose = newPurpose;
-    console.log(msg.sender,"set purpose to",purpose);
-    emit SetPurpose(msg.sender, purpose);
-  }
+struct Article {
+  string tittle;
+  string content;
+  address author;
+  uint256 timestamp;
+  uint256 id;
+}
+// create an array of article which would hold user data with there respective id
+Article[] public articles;
+    // Create your contract body with publish article
+function publishArticle(string memory _tittle, string memory _content) public returns(uint256){
+  /// user created article with user address content and timestamp with user id
+  uint256 myId = articles.length;
+  Article memory myArticle = Article(_tittle, _content, msg.sender, now, myId);
+  articles.push(myArticle);
+  /// Return the id to the users
+  return myId;
+  
+}
 
 }
